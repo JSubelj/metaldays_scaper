8eaf74828085ecb9ed13b8c8d08ed53c4b963469
Jan Šubelj <jan.subelj010@gmail.com>
2018-06-04 12:22:27 +0200

    extendal

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import urllib3
 from bs4 import BeautifulSoup
 import json
 
+API_KEY="4fa44a95-35f9-4a13-97f8-7f306cb96903"
+
 if __name__ == "__main__":
     http = urllib3.PoolManager()
     html = http.request('GET', "http://www.metaldays.net/Line_up").data
     soup = BeautifulSoup(html, 'html.parser')
     with open("bands.csv", "w") as f:
-        f.write("Band,Country,MDGenre,MAGenre\n")
+        f.write("Band,Country,MDGenre,MAGenre,noOfAlbums,ytVideo\n")
 
     for a in soup.find_all("a", {'class': 'article regular'}):
         html_band = http.request('GET', a.get('href')).data
@@ -16,6 +18,10 @@ if __name__ == "__main__":
             .find("div", {"id": "page"}) \
             .find("div", {"class": "content_header overflow"}) \
             .find("div", {"class": "inner"})
+        try:
+            band_yt_video = soup_band.find("div", {"class": "content_video"}).find("iframe").get("src")
+        except:
+            band_yt_video = ""
         name = band_and_genre.h1.string
         country = band_and_genre.span.string.split("(")[1][:-1]
         genre = band_and_genre.span.string.split("(")[0][:-1]
@@ -23,7 +29,8 @@ if __name__ == "__main__":
         metal_archives_json = http.request("GET",
                                            "http://em.wemakesites.net/search/band_name/"
                                            + name.replace(" ","%20")
-                                           + "?api_key=0719b560-a154-4a9c-bae8-31f05f5e72c9").data.decode("utf-8")
+                                           + "?api_key="+API_KEY).data.decode("utf-8")
+
 
         array_bands = json.loads(metal_archives_json)['data']['search_results']
         correct_band = None
@@ -36,10 +43,24 @@ if __name__ == "__main__":
                     correct_band = b
                     break
 
+        no_of_albums = 0
         if len(array_bands) == 0:
             MAGenre = ""
         elif correct_band is not None:
             MAGenre = correct_band['genre']
+            metal_archives_json = http.request("GET",
+                                               "http://em.wemakesites.net/band/"
+                                               + correct_band['id']
+                                               + "?api_key=" + API_KEY).data.decode("utf-8")
+
+            band_discography = json.loads(metal_archives_json)['data']["discography"]
+
+            json.loads(metal_archives_json)['data'].keys()
+            for album in band_discography:
+                if album['type'].lower() == "Full-length".lower():
+                    no_of_albums +=1
+            #print(correct_band['name'])
+            #print(no_of_albums)
         else:
             print("\nCORRECT ME")
             print(country)
@@ -47,5 +68,5 @@ if __name__ == "__main__":
             MAGenre = ""
 
         with open("bands.csv","a") as f:
-            f.write("%s,%s,%s,%s\n" % (name, country, genre, MAGenre))
+            f.write("%s,%s,%s,%s,%d,%s\n" % (name, country, genre, MAGenre,no_of_albums,band_yt_video))
 
